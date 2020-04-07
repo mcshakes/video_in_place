59ed88a4e97398b297d411a41d0bc69168b07d31
mcshakes <efmccarthy26@gmail.com>
2020-04-07 15:51:29 -0600

    Good starting point with everything hooked up. Next stop: a room

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import VideoComponent from './Video';
 import './App.css';
 
 class App extends React.Component {
@@ -30,29 +31,8 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <VideoComponent />
       </div>
     );
   }
---
+++ client/src/Video.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TwilioVideo from 'twilio-video';
+import axios from 'axios';
+
+class Video extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+            <div>Video Component</div>
+        );
+    }
+
+}
+
+export default Video;
